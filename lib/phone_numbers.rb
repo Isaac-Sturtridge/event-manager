42c12d9d320363b489e04af892203527f60bfc70
izzy <eyes330@yahoo.co.uk>
2023-02-02 16:37:01 +0800

    code readability changes

--- lib/phone_numbers.rb
+++ lib/phone_numbers.rb
@@ -1,6 +1,5 @@
 require 'csv'
 
-
 puts 'Phone Trimmer Initialized!'
 
 contents = CSV.open(
@@ -10,17 +9,16 @@ contents = CSV.open(
 )
 
 contents.each do |row|
-    phone_number = row[5]
+  phone_number = row[5]
 
-    phone_number = phone_number.strip
-    phone_number = phone_number.gsub(/\D/, '')
+  phone_number = phone_number.strip
+  phone_number = phone_number.gsub(/\D/, '')
 
-    if phone_number.length < 10 || phone_number.length > 11 || phone_number.length == 11 && phone_number[0] != 1
-        puts 'Bad number!'
-    elsif phone_number.length == 11 && phone_number[0] == 1
-        phone_number = phone_number[1..11]
-    else 
-      puts phone_number
-    end 
-    
+  if phone_number.length < 10 || phone_number.length > 11 || phone_number.length == 11 && phone_number[0] != 1
+    puts 'Bad number!'
+  elsif phone_number.length == 11 && phone_number[0] == 1
+    phone_number = phone_number[1..11]
+  else
+    puts phone_number
+  end
 end
--- lib/time_targeting.rb
+++ lib/time_targeting.rb
@@ -2,41 +2,40 @@ require 'csv'
 require 'date'
 require 'time'
 
-
 puts 'Time Targeting Initialized!'
 
 contents = CSV.open(
     'event_attendees.csv',
     headers: true, 
     header_converters: :symbol
-)
+  )
 
 hours = {}
 days = {}
 
 contents.each do |row|
-    timedate = row[1]
-    if timedate.nil?
-        timedate = Time.now
-    end
-    puts timedate
-    time = Time.strptime(timedate, "%m/%d/%y %k:%M")
-    date = Date.strptime(timedate, "%m/%d/%y %k:%M")
-    if hours.include?(time.hour)
-        hours[time.hour] += 1
-    else 
-        hours[time.hour] = 1
-    end
+  timedate = row[1]
+  if timedate.nil?
+    timedate = Time.now
+  end
+  puts timedate
+  time = Time.strptime(timedate, "%m/%d/%y %k:%M")
+  date = Date.strptime(timedate, "%m/%d/%y %k:%M")
+  if hours.include?(time.hour)
+    hours[time.hour] += 1
+  else 
+    hours[time.hour] = 1
+  end
 
-    if days.include?(date.wday)
-        days[date.wday] += 1
-    else 
-        days[date.wday] = 1
-    end 
+  if days.include?(date.wday)
+    days[date.wday] += 1
+  else
+    days[date.wday] = 1
+  end
 end
 
 weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-day_max = days.max_by{|k,v| v}[0]
+day_max = days.max_by { |_k, v| v }[0]
 
-puts "The most common hour for registration is #{hours.max_by{|k,v| v}[0]} o'clock, registering #{hours.max_by{|k,v| v}[1]} times"
+puts "The most common hour for registration is #{hours.max_by { |_k, v| v }[0]} o'clock, registering #{hours.max_by{|k,v| v}[1]} times"
 puts "The most common day for registration is #{weekdays[day_max]}"
